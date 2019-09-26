bbefb207a1f7918fb503fc36339e1d708e5c9bf4
Fernando Marques <frpm1001@gmail.com>
2019-09-25 22:36:30 -0300

    Add files via upload

--- amb.js
+++ amb.js
@@ -81,25 +81,28 @@ document.getElementById("btnDev").onclick = function(){
     var dcProc = document.getElementById("dcProcD");
     var dcMae = document.getElementById("dcMaeD");
     var dcHD = document.getElementById("dcHDD");
+    var dcVid = document.getElementById("dcVidD");
     var dcGab = document.getElementById("dcGabD");
 
     var dcTotal = document.getElementById("dcTotalD");
     var valorTotal = document.getElementById("valorTotalD");
 
     const qtPc = document.getElementById("iptDev").value;
-    const a = Math.trunc(qtPc*1.0 + 30);
-    const b = Math.trunc(qtPc*0.8 + 30);
-    const c = Math.trunc(qtPc*0.3 + 20);
-    const d = Math.trunc(qtPc*0.4 + 16);
-    const e = Math.trunc(qtPc*0.6 + 18);
+    const a = Math.trunc(qtPc*0.8 + 22);
+    const b = Math.trunc(qtPc*1.4 + 55);
+    const c = Math.trunc(qtPc*0.5 + 20);
+    const d = Math.trunc(qtPc*0.7 + 26);
+    const e = Math.trunc(qtPc*0.6 + 28);
+    const f = Math.trunc(qtPc*1.1 + 40);
     dcOffice.innerHTML = a + ",00";
     dcProc.innerHTML = b + ",00";
     dcMae.innerHTML = c + ",00";
     dcHD.innerHTML = d + ",00";
     dcGab.innerHTML = e + ",00";
+    dcVid.innerHTML = f + ",00";
 
-    dcTotal.innerHTML = a+b+c+d+e + ",00";
-    valorTotal.innerHTML = 1400 - (a+b+c+d+e) + ",00";
+    dcTotal.innerHTML = a+b+c+d+e+f + ",00";
+    valorTotal.innerHTML = 2600 - (a+b+c+d+e+f) + ",00";
 };
 
 //_____________________________________DESIGN________________________________________
@@ -109,24 +112,28 @@ document.getElementById("btnDes").onclick = function(){
     var dcMae = document.getElementById("dcMaeE");
     var dcHD = document.getElementById("dcHDE");
     var dcGab = document.getElementById("dcGabE");
+    var dcVid = document.getElementById("dcVidE");
 
     var dcTotal = document.getElementById("dcTotalE");
     var valorTotal = document.getElementById("valorTotalE");
 
     const qtPc = document.getElementById("iptDes").value;
-    const a = Math.trunc(qtPc*1.0 + 30);
-    const b = Math.trunc(qtPc*0.8 + 30);
-    const c = Math.trunc(qtPc*0.3 + 20);
+    const a = Math.trunc(qtPc*1.6 + 30);
+    const b = Math.trunc(qtPc*1.4 + 55);
+    const c = Math.trunc(qtPc*0.5 + 20);
     const d = Math.trunc(qtPc*0.4 + 16);
-    const e = Math.trunc(qtPc*0.6 + 18);
+    const e = Math.trunc(qtPc*0.6 + 28);
+    const f = Math.trunc(qtPc*1.8 + 65);
     dcOffice.innerHTML = a + ",00";
     dcProc.innerHTML = b + ",00";
     dcMae.innerHTML = c + ",00";
     dcHD.innerHTML = d + ",00";
     dcGab.innerHTML = e + ",00";
+    dcVid.innerHTML = f + ",00";
+    
 
-    dcTotal.innerHTML = a+b+c+d+e + ",00";
-    valorTotal.innerHTML = 1400 - (a+b+c+d+e) + ",00";
+    dcTotal.innerHTML = a+b+c+d+e+f + ",00";
+    valorTotal.innerHTML = 3100 - (a+b+c+d+e+f) + ",00";
 };
 
 //_____________________________________SERVER________________________________________
